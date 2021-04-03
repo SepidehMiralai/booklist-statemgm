b5e4f7c98949d0213b846627317282e59318823f
Sepideh Miralaei <sepidehmiralaei@Sepidehs-MacBook-Pro.local>
2021-04-02 20:47:29 -0400

    minor change

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,7 @@ const Nav = () =>  {
   const [books, setbooks] = useContext(BookContext);
   return (
     <div className = "navbar ">
-      <h3> Sepid's list of favorite books: </h3>
+      <h3> My list of favorite books: </h3>
       <h3> Number of Books: {books.length} </h3>
     </div>
   )
